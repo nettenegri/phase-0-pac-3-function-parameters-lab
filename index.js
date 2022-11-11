14fcaea93b6cb2363347b888a2b4eff21a657a23
nettenegri <nettenegri@gmail.com>
2022-11-10 20:39:04 -0700

    commit tree be45073a0ea3db5d6875cdce4e6a3ee15ccb7108

---
+++ index.js
@@ -0,0 +1,12 @@
+function introduction(firstName) {
+  console.log(`Hi, my name is ${firstName}!`);
+}
+introduction("Aki");
+
+function introductionWithLanguage(name, language) {
+  console.log(
+    "Hi, my name is ${value1} and I learning to program in ${value2}."
+  );
+  introductionWithLanguage("Aki", "English");
+}
+return string.toUpperCase();
